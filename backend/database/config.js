47df4c99d0a3100f4e417e5cb8a54a6329a97143
Davis Kabiswa <daviskabiswa@Daviss-MacBook-Pro.local>
2019-08-21 08:35:03 +0200

    bug(Database): Use database connection string set as enviromental variable[Finishes #168021502]

---
+++ backend/database/config.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+module.exports = {
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+  },
+  test: {
+    use_env_variable: 'TEST_DATABASE_URL',
+    dialect: 'postgres',
+  },
+};
