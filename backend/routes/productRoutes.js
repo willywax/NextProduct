840114aeee0468889786f0c2dcce480773bef281
BAHATI Robben <lux@BAHATIs-MacBook-Pro.local>
2019-08-23 12:59:25 +0200

    feat(own products): a user can see his own products
    - as a user, I can see a list of the products that i have posted
    - the login is required(protected route)
    
    [Finishes #167944589]
    
    feat(own products): a user can see his own products
    
    - as a user, I can see a list of the products that i have posted
    - the login is required(protected route)
    
    [Finishes #167944589]

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -12,6 +12,7 @@ router.use(fileupload({
 router.post('/', validation.productValidation, ProductController.addProduct);
 router.get('/', ProductController.getAllProducts);
 router.patch('/:id', validation.productUpdateValidation, ProductController.updateProduct);
+router.get('/myproducts', ProductController.getMyProducts);
 router.get('/:id', validation.productIdValidate, ProductController.viewProduct);
 
 export default router;
